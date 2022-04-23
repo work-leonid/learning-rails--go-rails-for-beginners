44fea338211682ab948729955771017ed49e0ed1
Leonid Nazarov <work.leonid@gmail.com>
2022-04-23 15:43:37 +0300

    lesson 14

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   root "main#index"
   get 'about', to: 'about#index'
   get 'sign_up', to: 'registrations#new'
-  post 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
 end
