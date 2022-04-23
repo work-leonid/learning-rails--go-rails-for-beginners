3443739ee1ad51b5784c35ae22b765014a2fbf8d
Leonid Nazarov <work.leonid@gmail.com>
2022-04-23 13:19:39 +0300

    lesson 13

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "main#index"
   get 'about', to: 'about#index'
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#new'
 end
